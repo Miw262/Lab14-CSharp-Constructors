fe710d38fbc59be21772b43baeca2ac8325e93ec
Miw262 <chauat246@gmail.com>
2025-03-24 18:38:15 +0700

    1

---
+++ Lab13_Ex04/Program.cs
@@ -0,0 +1,41 @@
+﻿var circle = new Circle();
+var rectangle = new Rectangle();
+var triangle = new Triangle();
+
+class Shape
+{
+    private int? NumOfSide;
+    // เปลี่ยนเป็น protected เพื่อให้คลาสลูกสามารถเข้าถึงได้
+    protected Shape()
+    {
+        System.Console.WriteLine("This is some shape with unknown side");
+    }
+    public Shape(int NumOfSide)
+    {
+        System.Console.WriteLine($"This is some shape with {NumOfSide} sides" );
+    }
+}
+
+class Circle : Shape
+{
+    public Circle() : base()
+    {
+        System.Console.WriteLine("This is a circle");
+    }
+}
+
+class Rectangle : Shape
+{
+    public Rectangle() : base(4)
+    {
+        System.Console.WriteLine("This is a rectangle");
+    }
+}
+
+class Triangle : Shape
+{
+    public Triangle() : base(3)
+    {
+        System.Console.WriteLine("This is a triangle");
+    }
+}
---
+++ Lab13_Ex05/Program.cs
@@ -0,0 +1,44 @@
+﻿var circle = new Circle();
+var rectangle = new Rectangle();
+var triangle = new Triangle();
+
+class Shape
+{
+    private int? NumOfSide;
+    
+    // ทำให้คอนสตรัคเตอร์ที่ไม่มีพารามิเตอร์เป็น protected
+    protected Shape()
+    {
+    }
+
+    // คอนสตรัคเตอร์ที่รับพารามิเตอร์
+    public Shape(int NumOfSide) : this()
+    {
+        this.NumOfSide = NumOfSide;
+        System.Console.WriteLine($"This is some shape with {NumOfSide} sides");
+    }
+}
+
+class Circle : Shape
+{
+    public Circle() : base(0)  // ค่า 0 สำหรับจำนวนข้างของวงกลม
+    {
+        System.Console.WriteLine("This is a circle");
+    }
+}
+
+class Rectangle : Shape
+{
+    public Rectangle() : base(4)  // ค่า 4 สำหรับจำนวนข้างของสี่เหลี่ยม
+    {
+        System.Console.WriteLine("This is a rectangle");
+    }
+}
+
+class Triangle : Shape
+{
+   public Triangle() : base(3)  // ค่า 3 สำหรับจำนวนข้างของสามเหลี่ยม
+   {
+        System.Console.WriteLine("This is a triangle");
+   }
+}
